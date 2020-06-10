7c83483b244b791e85cb2701b8c9faec5d37cc25
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2020-06-10 12:35:55 -0400

    added test_mcmicro_to_scimap

--- scimap/tests/test_preprocessing.py
+++ scimap/tests/test_preprocessing.py
@@ -3,17 +3,21 @@
 """
 Created on Wed Jun 10 09:32:49 2020
 @author: Ajit Johnson Nirmal
+Tests
 """
 
 import pytest
+import sys, os
 
+@pytest.fixture
+def adata():
+    from scimap.preprocessing._mcmicro_to_scimap import mcmicro_to_scimap
+    image_path = [os. getcwd() + '/scimap/tests/_data/example_data.csv']
+    adata = mcmicro_to_scimap (image_path, drop_markers=['BG1', 'BG2', 'BG3'])
+    return adata
 
-#def test_mcmicro_to_scimap(data):
-#    from scimap.preprocessing._mcmicro_to_scimap import mcmicro_to_scimap
-#    image_path = ['../_data/example_data.csv']
-#    adata = mcmicro_to_scimap (image_path)
-
-
-
-#image_path = ['/Users/aj/Dropbox (Partners HealthCare)/packages/scimap/scimap/tests/data/example_data.csv']
-#adata = sm.pp.mcmicro_to_scimap (image_path)
+    
+def test_mcmicro_to_scimap(adata):
+    assert adata.shape == (3029, 33)
+    
+    
