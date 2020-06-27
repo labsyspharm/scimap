3f1803339ca39acfee505a56352b425ca24a98a6
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2020-06-26 21:32:12 -0400

    mcmicro input

--- scimap/preprocessing/_mcmicro_to_scimap.py
+++ scimap/preprocessing/_mcmicro_to_scimap.py
@@ -12,10 +12,10 @@ import pandas as pd
 
 def mcmicro_to_scimap (image_path,remove_dna=True,remove_string_from_name=None,
                         log=True,drop_markers=None,random_sample=None,
-                        CellId='CellID',split='X_position',custom_imageid=None,
+                        CellId='CellID',split='X_centroid',custom_imageid=None,
                         min_cells=None):
     """
-    
+
     Parameters
     ----------
     image_path : list
@@ -23,10 +23,10 @@ def mcmicro_to_scimap (image_path,remove_dna=True,remove_string_from_name=None,
     remove_dna : bool, optional
         Remove the DNA channels from the final output. Looks for channels with the string 'dna' in it. The default is True.
     remove_string_from_name : string, optional
-        Used to celan up channel names. If a string is given, that particular string will be removed from all marker names. 
+        Used to celan up channel names. If a string is given, that particular string will be removed from all marker names.
         If multiple images are passed, just use the string that appears in the first image. The default is None.
-    islog : bool, optional
-        If the data is in log scale, passing true will convert it to natural scale. The default is True.
+    log : bool, optional
+        Log the data (log1p transformation will be applied). The default is True.
     drop_markers : list, optional
         List of markers to drop from the analysis. e.g. ["CD3D", "CD20"]. The default is None.
     random_sample : int, optional
@@ -35,12 +35,17 @@ def mcmicro_to_scimap (image_path,remove_dna=True,remove_string_from_name=None,
         Name of the column that contains the cell ID. The default is CellID.
     split : string, optional
         To split the CSV into counts table and meta data, pass in the name of the column
-        that immediately follows the marker quantification. The default is 'X_position'.
+        that immediately follows the marker quantification. The default is 'X_centroid'.
+    custom_imageid: string, optional
+        Pass a user defined Image ID. By default the name of the CSV file is used.
+    min_cells: int, optional
+        If these many cells are not in the image, the image will be dropped.
+        Particulary useful when importing multiple images.
 
     Returns
     -------
     AnnData Object
-        
+
     Example
     -------
     image_path = ['/Users/aj/whole_sections/PTCL1_450.csv',
@@ -48,18 +53,18 @@ def mcmicro_to_scimap (image_path,remove_dna=True,remove_string_from_name=None,
     adata = sm.pp.mcmicro_to_scimap (image_path, drop_markers= ['CD21', 'ACTIN'], random_sample=5000)
 
     """
-    
+
     # Import data based on the location provided
     def load_process_data (image):
         # Print the data that is being processed
-        print("Loading " + str(image.rsplit('/', 1)[-1]))       
-        d = pd.read_csv(image) 
+        print("Loading " + str(image.rsplit('/', 1)[-1]))
+        d = pd.read_csv(image)
         # If the data does not have a unique image ID column, add one.
         if 'ImageId' not in d.columns:
             if custom_imageid is not None:
                 imid = custom_imageid
             else:
-                #imid = random.randint(1000000,9999999)  
+                #imid = random.randint(1000000,9999999)
                 imid = str(image.rsplit('/', 1)[-1]).replace('.csv','')
             d['ImageId'] = imid
         # Unique name for the data
@@ -75,59 +80,59 @@ def mcmicro_to_scimap (image_path,remove_dna=True,remove_string_from_name=None,
         # Return data
         return d
     # Apply function to all images and create a master dataframe
-    r_load_process_data = lambda x: load_process_data(image=x) # Create lamda function 
-    all_data = list(map(r_load_process_data, list(image_path))) # Apply function 
-    
+    r_load_process_data = lambda x: load_process_data(image=x) # Create lamda function
+    all_data = list(map(r_load_process_data, list(image_path))) # Apply function
+
     # Merge all the data into a single large dataframe
     for i in range(len(all_data)):
         all_data[i].columns = all_data[0].columns
     entire_data = pd.concat(all_data, axis=0, sort=False)
-    
+
     #Remove the images that contain less than a defined threshold of cells (min_cells)
     if min_cells is not None:
         to_drop = entire_data['ImageId'].value_counts()[entire_data['ImageId'].value_counts() < min_cells].index
         entire_data = entire_data[~entire_data['ImageId'].isin(to_drop)]
         print('Removed Images that contained less than '+str(min_cells)+' cells: '+ str(to_drop.values))
-   
+
     # Split the data into expression data and meta data
     # Step-1 (Find the index of the column with name Area)
     split_idx = entire_data.columns.get_loc(split)
     meta = entire_data.iloc [:,split_idx:]
     # Step-2 (select only the expression values)
     entire_data = entire_data.iloc [:,:split_idx]
-    
+
     # Rename the columns of the data
     if remove_string_from_name is not None:
         entire_data.columns = entire_data.columns.str.replace(remove_string_from_name, '')
 
     # Save a copy of the column names in the uns space of ANNDATA
     markers = list(entire_data.columns)
-    
+
     # Remove DNA channels
     if remove_dna is True:
-        entire_data = entire_data.loc[:,~entire_data.columns.str.contains('dna', case=False)] 
-    
+        entire_data = entire_data.loc[:,~entire_data.columns.str.contains('dna', case=False)]
+
     # Drop unnecessary markers
     if drop_markers is not None:
         for i in drop_markers:
-            entire_data = entire_data.loc[:,~entire_data.columns.str.contains(i, case=False)] 
-    
+            entire_data = entire_data.loc[:,~entire_data.columns.str.contains(i, case=False)]
+
 
     # Randomly sample the data
     if random_sample is not None:
         entire_data = entire_data.sample(n=random_sample,replace=False)
-    
+
 
     # Create an anndata object
     adata = ad.AnnData(entire_data)
     adata.obs = meta
     adata.uns['all_markers'] = markers
-    
+
     # Add log data
     if log is True:
         adata.raw = adata
         adata.X = np.log1p(adata.X)
-    
-    
+
+
     # Return data
     return adata
--- scimap/tests/test_preprocessing.py
+++ scimap/tests/test_preprocessing.py
@@ -13,31 +13,28 @@ import sys, os
 def adata():
     from scimap.preprocessing._mcmicro_to_scimap import mcmicro_to_scimap
     image_path = [os. getcwd() + '/scimap/tests/_data/example_data.csv']
-    adata = mcmicro_to_scimap (image_path, drop_markers=['BG1', 'BG2', 'BG3'])
+    adata = mcmicro_to_scimap (image_path, split='X_position', drop_markers=['BG1', 'BG2', 'BG3'])
     return adata
 
-    
+
 def test_mcmicro_to_scimap(adata):
     assert adata.shape == (3029, 33)
-    
+
 def test_rescale_phenotype(adata):
     import pandas as pd
     import numpy as np
     from scimap.preprocessing._rescale import rescale
     from scimap.tools._phenotype_cells import phenotype_cells
-    
+
     # test rescaling data
     manual_gate = pd.DataFrame({'marker': ['CD3D', 'KI67'], 'gate': [7, 8]})
     adata = rescale (adata, gate=manual_gate, failed_markers=['CD20', 'CD21'])
     a = np.round(adata[:,'CD3D'].X[0],2)
-    
+
     # Load phenotype and test phenotyping
     phenotype = pd.read_csv(os. getcwd() + '/scimap/tests/_data/phenotype_workflow.csv')
-    adata = phenotype_cells (adata, phenotype=phenotype, gate = 0.5, label="phenotype") 
+    adata = phenotype_cells (adata, phenotype=phenotype, gate = 0.5, label="phenotype")
     b = adata.obs['phenotype'][0]
-    
+
     # test
     assert (a, b) == (0.37, 'M2 Macrophages')
-    
-
-    
