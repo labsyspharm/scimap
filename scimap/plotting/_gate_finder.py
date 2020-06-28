f743a4988919d5a6b39142125ebd38a1dd9ab1d9
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2020-06-28 18:45:06 -0400

    fixed XY_centroid

--- scimap/plotting/_gate_finder.py
+++ scimap/plotting/_gate_finder.py
@@ -13,9 +13,9 @@ import numpy as np
 
 def gate_finder (image_path, adata, marker_of_interest, from_gate = 6, to_gate = 8, increment = 0.1,
                  markers=None, channel_names = 'default',
-                 x='X_position',y='Y_position',point_size=10,image_id=None,seg_mask=None):
+                 x='X_centroid',y='Y_centroid',point_size=10,image_id=None,seg_mask=None):
     """
-    
+
 
     Parameters
     ----------
@@ -48,58 +48,58 @@ def gate_finder (image_path, adata, marker_of_interest, from_gate = 6, to_gate =
     Example
     -------
     image_path = '/Users/aj/Desktop/ptcl_tma/image.tif'
-    sm.pl.gate_finder (image_path, adata, marker_of_interest='CD45', 
+    sm.pl.gate_finder (image_path, adata, marker_of_interest='CD45',
                  from_gate = 6, to_gate = 8, increment = 0.1,
                  markers=['DNA10'], channel_names = 'default',
                  x='X_position',y='Y_position',point_size=10,
                  image_id= '77', seg_mask=None)
 
     """
-    
-    
-    
+
+
+
     # If no raw data is available make a copy
     if adata.raw is None:
         adata.raw = adata
-        
+
     # Copy of the raw data if it exisits
     if adata.raw is not None:
         adata.X = adata.raw.X
-        
+
     # Make a copy of the data with the marker of interest
     data = pd.DataFrame(np.log1p(adata.X), columns = adata.var.index, index= adata.obs.index)[[marker_of_interest]]
-    
+
     # Generate a dataframe with various gates
-    def gate (g, d): 
+    def gate (g, d):
         dd = d.values
         dd = np.where(dd < g, np.nan, dd)
         np.warnings.filterwarnings('ignore')
         dd = np.where(dd > g, 1, dd)
         dd = pd.DataFrame(dd, index = d.index, columns = ['gate-' + str(g)])
         return dd
-    
+
     # Identify the list of increments
     inc = list(np.arange (from_gate, to_gate, increment))
     inc = [round(num,3) for num in inc]
-    
+
     # Apply the function
-    r_gate = lambda x: gate(g=x, d=data) # Create lamda function 
-    gated_data = list(map(r_gate, inc)) # Apply function 
+    r_gate = lambda x: gate(g=x, d=data) # Create lamda function
+    gated_data = list(map(r_gate, inc)) # Apply function
     # Concat all the results into a single dataframe
     gates = pd.concat(gated_data, axis=1)
-    
+
     ##########################################################################
     # Visulaisation using Napari
     # Plot only the Image that is requested
     if image_id is not None:
-        adata = adata[adata.obs['ImageId'] == image_id] 
-    
+        adata = adata[adata.obs['ImageId'] == image_id]
+
     # Recover the channel names from adata
     if channel_names is 'default':
         channel_names = adata.uns['all_markers']
     else:
         channel_names = channel_names
-        
+
 
     # Index of the marker of interest and corresponding names
     if markers is None:
@@ -110,18 +110,18 @@ def gate_finder (image_path, adata, marker_of_interest, from_gate = 6, to_gate =
         channel_names = channel_names
     else:
         markers = [marker_of_interest] + list(markers)
-        idx = []  
+        idx = []
         for i in markers:
             idx.append(list(channel_names).index(i))
         channel_names = markers
-    
+
     # Load the image
     image = tiff.imread(image_path, key = idx)
-     
+
     # Load the segmentation mask
     if seg_mask is not None:
         seg_m = tiff.imread(seg_mask)
-    
+
     # Load the viewer
     viewer = napari.view_image(
     image,
@@ -129,29 +129,25 @@ def gate_finder (image_path, adata, marker_of_interest, from_gate = 6, to_gate =
     channel_axis=0,
     name = None if channel_names is None else channel_names,
     visible = False)
-    
+
     # Add the seg mask
     if seg_mask is not None:
-        viewer.add_labels(seg_m, 
+        viewer.add_labels(seg_m,
                           name='segmentation mask',
                           visible = False)
-    
+
     # subset the gates to include only the image of interest
     gates = gates.loc[adata.obs.index,]
-    
+
     # Add phenotype layer function
     def add_phenotype_layer (adata, gates, phenotype_layer,x,y,viewer,point_size):
-        cells = gates[gates[phenotype_layer] == 1].index        
+        cells = gates[gates[phenotype_layer] == 1].index
         coordinates = adata[cells]
         coordinates = pd.DataFrame({'y': coordinates.obs[y],'x': coordinates.obs[x]})
         points = coordinates.values.tolist()
         viewer.add_points(points, size=point_size, face_color='white',visible=False,name=phenotype_layer)
-        
-    # Run the function on all phenotypes    
+
+    # Run the function on all phenotypes
     for i in gates.columns:
         add_phenotype_layer (adata=adata, gates=gates, phenotype_layer=i, x=x, y=y, viewer=viewer,
                                                            point_size=point_size)
-        
-        
-    
-        
