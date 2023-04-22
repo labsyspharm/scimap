b4321bc9c0bfebd1b664881bede6ba460047850c
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2023-04-21 22:11:05 -0400

    adding ability supply custom color to image viewer function

--- scimap/plotting/_image_viewer.py
+++ scimap/plotting/_image_viewer.py
@@ -33,51 +33,58 @@ def image_viewer (image_path, adata, overlay=None, flip_y=True,
                     point_color=None,subset=None,imageid='imageid',seg_mask=None,**kwargs):
     """
 Parameters:
-    image_path : string  
+    image_path (str):  
         Location to the image file (TIFF, OME.TIFF, ZARR supported)
 
-    seg_mask: string  
+    seg_mask (str, optional):    
         Location to the segmentation mask file.
 
-    adata : AnnData Object  
+    adata (Ann Data Object):  
         
-    flip_y : bool, optional  
+    flip_y (bool, optional):  
         Flip the Y-axis if needed. Some algorithms output the XY with the Y-coordinates flipped.
         If the image overlays do not align to the cells, try again by setting this to `False`.
 
-    overlay : string, optional  
+    overlay (str, optional):  
         Name of the column with any categorical data such as phenotypes or clusters.
 
-    overlay_category : list, optional  
+    overlay_category (list, optional):  
         If only specfic categories within the overlay column is needed, pass their names as a list.
         If None, all categories will be used.
 
-    markers : list, optional  
+    markers (list, optional):  
         Markers to be included. If none, all markers will be displayed.
 
-    channel_names : list, optional  
+    channel_names (list, optional):  
         List of channels in the image in the exact order as image. The default is `adata.uns['all_markers']`
 
-    x_coordinate : string, optional  
+    x_coordinate (str, optional):    
         X axis coordinate column name in AnnData object.
 
-    y_coordinate : string, optional  
+    y_coordinate (str, optional):    
         Y axis coordinate column name in AnnData object.
 
-    point_size : int, optional  
+    point_size (int, optional):    
         point size in the napari plot.
+    
+    point_color (str, dict, optional):    
+        The default behavior is to assign auto colors, but you can also provide 
+        a color mapping using the point_color parameter. For instance, you can pass a 
+        dictionary that maps color values to specific categories (provided in the `overlay` parameter). 
+        Here is an example of such a color mapping: `point_color = {'cellTypeA': ['#FFFFFF'], 'cellTypeB': ['#000000']}`. 
+        A single color can also be provided like `point_color = 'white'`
 
-    imageid : string, optional  
+    imageid (str, optional):    
         Column name of the column containing the image id. 
 
-    subset : string, optional  
+    subset (str, optional):    
         imageid of a single image to be subsetted for analyis. Only useful when multiple images are being analyzed together.
 
     **kwargs  
         Other arguments that can be passed to napari viewer
     
 Returns:
-    Napari Viewer.
+    Napari Viewer (image viewer):  
 
 Example:
 ```python
@@ -167,7 +174,7 @@ Example:
             pyramid, multiscale=multiscale, channel_axis=0,
             visible=False, 
             name = None if channel_names is None else channel_names,
-            #**kwargs
+            **kwargs
         )
     
     # Operations on the ZARR image
@@ -184,7 +191,7 @@ Example:
         viewer.add_labels(seg_m, name='segmentation mask', visible=False)
 
     # Add phenotype layer function
-    def add_phenotype_layer (adata, overlay, phenotype_layer,x,y,viewer,point_size,point_color):
+    def add_phenotype_layer (adata, overlay, phenotype_layer,x,y,viewer,point_size,point_color,available_phenotypes):
         coordinates = adata[adata.obs[overlay] == phenotype_layer]
         # Flip Y AXIS if needed
         if flip_y is True:
@@ -197,6 +204,18 @@ Example:
         if point_color is None:
             r = lambda: random.randint(0,255) # random color generator
             point_color = '#%02X%02X%02X' % (r(),r(),r()) # random color generator
+        elif isinstance(point_color, dict):
+            # if dict identify the color for the given phenotype
+            # also if a color is not provided in the dict assign it to white
+            try:
+                point_color = point_color[available_phenotypes]
+            except KeyError:
+                point_color = 'white'
+                # if the dict has list, we need to account for it and so the following two lines
+                if isinstance(point_color, list):
+                    point_color = point_color[0]
+            
+        # check if point_color is a dict and if so isolate the color to the specific categoty
         viewer.add_points(points, size=point_size,face_color=point_color,visible=False,name=phenotype_layer)
 
     if overlay is not None:
@@ -210,4 +229,4 @@ Example:
         for i in available_phenotypes:
             add_phenotype_layer (adata=adata, overlay=overlay,
                                     phenotype_layer=i, x=x_coordinate, y=y_coordinate, viewer=viewer,
-                                    point_size=point_size,point_color=point_color)
+                                    point_size=point_size,point_color=point_color,available_phenotypes=i)
