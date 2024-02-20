68bf9f8cf75c3ffc0415550028a9170937ebfa3a
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2024-02-20 09:03:43 -0500

    np.seterr('ignore')

--- scimap/plotting/_gate_finder.py
+++ scimap/plotting/_gate_finder.py
@@ -27,77 +27,93 @@ import os
 cache = Cache(2e9)  # Leverage two gigabytes of memory
 cache.register()
 
-def gate_finder (image_path, adata, marker_of_interest, layer='raw', log=True,
-                 from_gate = 6, to_gate = 8, increment = 0.1,
-                 markers=None, channel_names = 'default', flip_y=True,
-                 x_coordinate='X_centroid',y_coordinate='Y_centroid',
-                 point_size=10,imageid='imageid',subset=None,seg_mask=None,**kwargs):
+
+def gate_finder(
+    image_path,
+    adata,
+    marker_of_interest,
+    layer='raw',
+    log=True,
+    from_gate=6,
+    to_gate=8,
+    increment=0.1,
+    markers=None,
+    channel_names='default',
+    flip_y=True,
+    x_coordinate='X_centroid',
+    y_coordinate='Y_centroid',
+    point_size=10,
+    imageid='imageid',
+    subset=None,
+    seg_mask=None,
+    **kwargs,
+):
     """
-Parameters:
-    image_path (str):  
-        Location to the image file.
+    Parameters:
+        image_path (str):
+            Location to the image file.
 
-    adata : Ann Data Object  
+        adata : Ann Data Object
 
-    marker_of_interest (str):  
-        Marker for which gate is to be defined e.g. 'CD45'.
-    
-    layer (str): 
-        The layer in adata.layers that contains the expression data to gate. 
-        If None, adata.X is used. use `raw` to use the data stored in `adata.raw.X`
-    
-    log (bool):  
-        Log transform the data before gating.
+        marker_of_interest (str):
+            Marker for which gate is to be defined e.g. 'CD45'.
 
-    from_gate (int):   
-        Start value gate of interest.
+        layer (str):
+            The layer in adata.layers that contains the expression data to gate.
+            If None, adata.X is used. use `raw` to use the data stored in `adata.raw.X`
 
-    to_gate (int):    
-        End value of the gate of interest.
-        
-    flip_y (bool):  
-        Flip the Y-axis if needed. Some algorithms output the XY with the Y-coordinates flipped.
-        If the image overlays do not align to the cells, try again by setting this to `False`.
+        log (bool):
+            Log transform the data before gating.
 
-    increment (float):  
-        Increments between the start and end values.
+        from_gate (int):
+            Start value gate of interest.
 
-    markers (str):  
-        Additional markers to be included in the plot for evaluation.
+        to_gate (int):
+            End value of the gate of interest.
 
-    channel_names (list):  
-        List of channels in the image in the exact order as image. The default is `adata.uns['all_markers']`
+        flip_y (bool):
+            Flip the Y-axis if needed. Some algorithms output the XY with the Y-coordinates flipped.
+            If the image overlays do not align to the cells, try again by setting this to `False`.
 
-    x_coordinate (str):  
-        X axis coordinate column name in AnnData object.
+        increment (float):
+            Increments between the start and end values.
 
-    y_coordinate (str):  
-        Y axis coordinate column name in AnnData object.
+        markers (str):
+            Additional markers to be included in the plot for evaluation.
 
-    point_size (int):  
-        point size in the napari plot.
+        channel_names (list):
+            List of channels in the image in the exact order as image. The default is `adata.uns['all_markers']`
 
-    imageid (str):  
-        Column name of the column containing the image id.
+        x_coordinate (str):
+            X axis coordinate column name in AnnData object.
 
-    subset (str):  
-        imageid of a single image to be subsetted for analyis.
+        y_coordinate (str):
+            Y axis coordinate column name in AnnData object.
 
-    seg_mask (str):  
-        Location to the segmentation mask file.
+        point_size (int):
+            point size in the napari plot.
 
-    **kwargs  
-        Other arguments that can be passed to napari viewer.
+        imageid (str):
+            Column name of the column containing the image id.
 
-Example:
-```python
-    image_path = '/Users/aj/Desktop/ptcl_tma/image.ome.tif'
-    sm.pl.gate_finder (image_path, adata, marker_of_interest='CD45',
-                 from_gate = 6, to_gate = 8, increment = 0.1,
-                 markers=['DNA10'], channel_names = 'default',
-                 x_coordinate='X_position',y_coordinate='Y_position',point_size=10,
-                 subset= '77', seg_mask=None)
-```
+        subset (str):
+            imageid of a single image to be subsetted for analyis.
+
+        seg_mask (str):
+            Location to the segmentation mask file.
+
+        **kwargs
+            Other arguments that can be passed to napari viewer.
+
+    Example:
+    ```python
+        image_path = '/Users/aj/Desktop/ptcl_tma/image.ome.tif'
+        sm.pl.gate_finder (image_path, adata, marker_of_interest='CD45',
+                     from_gate = 6, to_gate = 8, increment = 0.1,
+                     markers=['DNA10'], channel_names = 'default',
+                     x_coordinate='X_position',y_coordinate='Y_position',point_size=10,
+                     subset= '77', seg_mask=None)
+    ```
     """
 
     # If no raw data is available make a copy
@@ -106,61 +122,66 @@ Example:
 
     # subset data if neede
     if subset is not None:
-        if isinstance (subset, str):
+        if isinstance(subset, str):
             subset = [subset]
         if layer == 'raw':
-            bdata=adata.copy()
+            bdata = adata.copy()
             bdata.X = adata.raw.X
             bdata = bdata[bdata.obs[imageid].isin(subset)]
         else:
-            bdata=adata.copy()
+            bdata = adata.copy()
             bdata = bdata[bdata.obs[imageid].isin(subset)]
     else:
-        bdata=adata.copy()
-              
+        bdata = adata.copy()
+
     # isolate the data
     if layer is None:
-        data = pd.DataFrame(bdata.X, index=bdata.obs.index, columns=bdata.var.index)[[marker_of_interest]]
+        data = pd.DataFrame(bdata.X, index=bdata.obs.index, columns=bdata.var.index)[
+            [marker_of_interest]
+        ]
     elif layer == 'raw':
-        data = pd.DataFrame(bdata.raw.X, index=bdata.obs.index, columns=bdata.var.index)[[marker_of_interest]]
+        data = pd.DataFrame(
+            bdata.raw.X, index=bdata.obs.index, columns=bdata.var.index
+        )[[marker_of_interest]]
     else:
-        data = pd.DataFrame(bdata.layers[layer], index=bdata.obs.index, columns=bdata.var.index)[[marker_of_interest]]
-    
+        data = pd.DataFrame(
+            bdata.layers[layer], index=bdata.obs.index, columns=bdata.var.index
+        )[[marker_of_interest]]
+
     if log is True:
         data = np.log1p(data)
-        
 
     # Copy of the raw data if it exisits
-    #if adata.raw is not None:
+    # if adata.raw is not None:
     #    adata.X = adata.raw.X
-        
+
     # Plot only the Image that is requested
-    #if subset is not None:
+    # if subset is not None:
     #    adata = adata[adata.obs[imageid] == subset]
 
     # Make a copy of the data with the marker of interest
-    #data = pd.DataFrame(np.log1p(adata.X), columns = adata.var.index, index= adata.obs.index)[[marker_of_interest]]
+    # data = pd.DataFrame(np.log1p(adata.X), columns = adata.var.index, index= adata.obs.index)[[marker_of_interest]]
 
     # Generate a dataframe with various gates
-    def gate (g, d):
+    def gate(g, d):
         dd = d.values
         dd = np.where(dd < g, np.nan, dd)
-        np.warnings.filterwarnings('ignore')
+        # np.warnings.filterwarnings('ignore')
+        np.seterr('ignore')
         dd = np.where(dd > g, 1, dd)
-        dd = pd.DataFrame(dd, index = d.index, columns = ['gate-' + str(g)])
+        dd = pd.DataFrame(dd, index=d.index, columns=['gate-' + str(g)])
         return dd
 
     # Identify the list of increments
-    inc = list(np.arange (from_gate, to_gate, increment))
-    inc = [round(num,3) for num in inc]
+    inc = list(np.arange(from_gate, to_gate, increment))
+    inc = [round(num, 3) for num in inc]
 
     # Apply the function
-    r_gate = lambda x: gate(g=x, d=data) # Create lamda function
-    gated_data = list(map(r_gate, inc)) # Apply function
+    r_gate = lambda x: gate(g=x, d=data)  # Create lamda function
+    gated_data = list(map(r_gate, inc))  # Apply function
     # Concat all the results into a single dataframe
     gates = pd.concat(gated_data, axis=1)
-    
-  
+
     # Recover the channel names from adata
     if channel_names == 'default':
         channel_names = adata.uns['all_markers']
@@ -170,56 +191,55 @@ Example:
     # if markers is a string convert to list
     if isinstance(markers, str):
         markers = [markers]
-        
+
     # Index of the marker of interest and corresponding names
     if markers is not None:
         markers.extend([marker_of_interest])
-        idx = np.where(np.isin(channel_names,markers))[0]
+        idx = np.where(np.isin(channel_names, markers))[0]
         channel_names = [channel_names[i] for i in idx]
     else:
         idx = list(range(len(channel_names)))
         channel_names = channel_names
-
 
     # Load the segmentation mask
     if seg_mask is not None:
         seg_m = tiff.imread(seg_mask)
         if (len(seg_m.shape) > 2) and (seg_m.shape[0] > 1):
             seg_m = seg_m[0]
-        
 
     ##########################################################################
     # Visulaisation using Napari
-    
+
     # load OME TIFF
-    if os.path.isfile(image_path) is True: 
-        # Load the image    
+    if os.path.isfile(image_path) is True:
+        # Load the image
         image = tiff.TiffFile(image_path, is_ome=False)
-        z = zarr.open(image.aszarr(), mode='r') # convert image to Zarr array
+        z = zarr.open(image.aszarr(), mode='r')  # convert image to Zarr array
         # Identify the number of pyramids and number of channels
-        n_levels = len(image.series[0].levels) # pyramid  
+        n_levels = len(image.series[0].levels)  # pyramid
         # If and if not pyramids are available
         if n_levels > 1:
             pyramid = [da.from_zarr(z[i]) for i in range(n_levels)]
             multiscale = True
         else:
             pyramid = da.from_zarr(z)
-            multiscale = False   
+            multiscale = False
         # subset channels of interest
         if markers is not None:
             if n_levels > 1:
-                for i in range(n_levels-1):
+                for i in range(n_levels - 1):
                     pyramid[i] = pyramid[i][idx, :, :]
-                n_channels = pyramid[0].shape[0] # identify the number of channels
+                n_channels = pyramid[0].shape[0]  # identify the number of channels
             else:
                 pyramid = pyramid[idx, :, :]
-                n_channels = pyramid.shape[0] # identify the number of channels
+                n_channels = pyramid.shape[0]  # identify the number of channels
         else:
             if n_levels > 1:
                 n_channels = pyramid[0].shape[0]
             else:
-                n_channels = pyramid.shape[0]\
-        # check if channel names have been passed to all channels
+                n_channels = pyramid.shape[
+                    0
+                ]  # check if channel names have been passed to all channels
         if channel_names is not None:
             assert n_channels == len(channel_names), (
                 f'number of channel names ({len(channel_names)}) must '
@@ -228,21 +248,25 @@ Example:
 
         # Load the viewer
         viewer = napari.view_image(
-        pyramid,
-        channel_axis = 0,
-        multiscale=multiscale,
-        name = None if channel_names is None else channel_names,
-        visible = False, **kwargs)
-
+            pyramid,
+            channel_axis=0,
+            multiscale=multiscale,
+            name=None if channel_names is None else channel_names,
+            visible=False,
+            **kwargs,
+        )
 
     # Operations on the ZARR image
     # check the format of image
-    if os.path.isfile(image_path) is False: 
-        #print(image_path)
+    if os.path.isfile(image_path) is False:
+        # print(image_path)
         viewer = napari.Viewer()
-        viewer.open(image_path, multiscale=True,
-                    visible=False,
-                    name = None if channel_names is None else channel_names)
+        viewer.open(
+            image_path,
+            multiscale=True,
+            visible=False,
+            name=None if channel_names is None else channel_names,
+        )
 
     # Add the seg mask
     if seg_mask is not None:
@@ -252,25 +276,40 @@ Example:
     gates = gates.loc[bdata.obs.index,]
 
     # Add gating layer
-    def add_phenotype_layer (adata, gates, phenotype_layer,x,y,viewer,point_size):
+    def add_phenotype_layer(adata, gates, phenotype_layer, x, y, viewer, point_size):
         cells = gates[gates[phenotype_layer] == 1].index
         coordinates = adata[cells]
         # Flip Y axis if needed
         if flip_y is True:
-            coordinates = pd.DataFrame({'y': coordinates.obs[y],'x': coordinates.obs[x]})
-        else:  
-            coordinates = pd.DataFrame({'x': coordinates.obs[x],'y': coordinates.obs[y]})
-        #points = coordinates.values.tolist()
+            coordinates = pd.DataFrame(
+                {'y': coordinates.obs[y], 'x': coordinates.obs[x]}
+            )
+        else:
+            coordinates = pd.DataFrame(
+                {'x': coordinates.obs[x], 'y': coordinates.obs[y]}
+            )
+        # points = coordinates.values.tolist()
         points = coordinates.values
-        #import time
-        #start = time.time()
-        viewer.add_points(points, size=point_size, face_color='white',visible=False,name=phenotype_layer)
-        #stop = time.time()
-        #print(stop-start)
-        
+        # import time
+        # start = time.time()
+        viewer.add_points(
+            points,
+            size=point_size,
+            face_color='white',
+            visible=False,
+            name=phenotype_layer,
+        )
+        # stop = time.time()
+        # print(stop-start)
 
     # Run the function on all gating layer
     for i in gates.columns:
-        add_phenotype_layer (adata=bdata, gates=gates, 
-                             phenotype_layer=i, x=x_coordinate, y=y_coordinate, 
-                             viewer=viewer, point_size=point_size)
+        add_phenotype_layer(
+            adata=bdata,
+            gates=gates,
+            phenotype_layer=i,
+            x=x_coordinate,
+            y=y_coordinate,
+            viewer=viewer,
+            point_size=point_size,
+        )
