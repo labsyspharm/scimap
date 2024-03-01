bc7af46c25ff70f6a9c5326c159ee77ea4ad74b3
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2024-03-01 16:52:10 -0500

    spatial_pscore plot doc strings

--- scimap/plotting/_spatial_pscore.py
+++ scimap/plotting/_spatial_pscore.py
@@ -5,8 +5,13 @@
 
 """
 !!! abstract "Short Description"
-    `sm.pl.spatial_pscore`: The function allows users to plot proximity volume and density scores. 
-    Run `sm.tl.spatial_pscore` before running this function. 
+    `sm.pl.spatial_pscore`: This function offers a visual representation of 
+    proximity volume and density scores, essential for understanding the spatial 
+    relationships and interactions among cell types or phenotypes within tissue samples. 
+    To ensure accurate and meaningful visualizations, it is crucial to compute 
+    these scores beforehand using `sm.tl.spatial_pscore`. Through customizable bar 
+    plots, users can delve into the intricacies of spatial co-occurrence patterns, 
+    facilitating deeper insights into the cellular microenvironment.
 
 ## Function
 """
@@ -16,40 +21,53 @@ import seaborn as sns; sns.set_theme(style='white', color_codes=True)
 import matplotlib.pyplot as plt
 
 # Function
-def spatial_pscore (adata, label='spatial_pscore', plot_score='both', 
+def spatial_pscore (adata, 
+                    label='spatial_pscore', 
+                    plot_score='both', 
                     order_xaxis = None,
                     color='grey', **kwargs):
     """
 Parameters:
+        adata (anndata.AnnData):  
+            The annotated data matrix with spatial proximity scores.
 
-    adata : AnnData object
-    
-    label : string, optional  
-        The label under which the data is saved. This is the same `label` parameter 
-        passed when running the `sm.tl.spatial_pscore` function.
+        label (str, optional):  
+            The label/key used to access the spatial proximity scores stored in `adata.uns`. 
+            This should match the label used during the `sm.tl.spatial_pscore` computation. Default is 'spatial_pscore'.
 
-    plot_score : string, optional  
-        Three option are available. 
-        A) Plot only the *Proximity Density* by passing in `Proximity Density`  
-        B) Plot only the *Proximity Volume* by passing in `Proximity Volume`  
-        C) Plot both side by side by passing `both`
+        plot_score (str, optional):  
+            Determines which score(s) to plot. Options are:
+            - 'Proximity Density' for plotting only the Proximity Density scores,
+            - 'Proximity Volume' for plotting only the Proximity Volume scores,
+            - 'both' for plotting both scores side by side. Default is 'both'.
 
-    order_xaxis : list, optional  
-        If the user wants to re-order the x-axis, pass all the names in the x-axis
-        in the desired order as a list. e.g. ['ROI2', 'ROI4', "ROI1"] 
+        order_xaxis (list, optional):  
+            Custom order for the x-axis categories. Pass a list of category names in the desired order.
+            This can be useful for comparing specific regions or samples in a specific sequence.
 
-    color : string, optional  
-        Color of the bars.
+        color (str, optional):  
+            Color to use for the bar plots. This can enhance plot readability or align with publication themes.
 
-    **kwargs : string  
-        Other arguments that can be passed into `sns.barplot`
+        **kwargs:  
+            Additional keyword arguments passed directly to seaborn's barplot function, allowing for further customization of the plots.
+
+Returns:
+        Plot (matplotlib): 
+            Displays the generated bar plots.
 
 Example:
-```python
-    # Plot only `Proximity Volume` scores
-    sm.pl.spatial_pscore (adata, color='Black', 
-    plot_score='Proximity Volume', order_xaxis=['ROI2', 'ROI4', "ROI1"])
-```
+    ```python
+    
+    # Basic visualization of both Proximity Density and Proximity Volume
+    sm.pl.spatial_pscore(adata, label='spatial_pscore', plot_score='both', color='skyblue')
+
+    # Customized plot for Proximity Density with ordered x-axis and specific color
+    sm.pl.spatial_pscore(adata, plot_score='Proximity Density', order_xaxis=['Sample1', 'Sample2', 'Sample3'], color='salmon', edgecolor: 'black'})
+
+    # Focused plot on Proximity Volume with seaborn customization through kwargs
+    sm.pl.spatial_pscore(adata, plot_score='Proximity Volume', color='lightgreen', saturation: 0.8, alpha: 0.7})
+    
+    ```
     """
     
     
